3067ce8e40f85cb9901877eed305036ffe6c54bf
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2024-09-04 23:08:57 +0200

    test

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -10,18 +10,27 @@ LOG_FILE="$HOME/log/htmx_deploy.log"  # Centralized log file
 # Ensure log directory exists
 mkdir -p "$HOME/log"
 
-# Logging function
+# Trap to catch errors and cleanup
+trap 'cleanup_on_error' ERR
+
+# Logging function with log levels
 log() {
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG_FILE"
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] [$2] $1" | tee -a "$LOG_FILE"
+}
+
+cleanup_on_error() {
+    log "An error occurred during the deployment. Please check the logs." "ERROR"
+    deactivate_venv
+    exit 1
 }
 
 # Function to activate the fixed virtual environment
 activate_venv() {
     if [ -d "$VENV_DIR" ]; then
-        log "Activating the virtual environment at $VENV_DIR..."
+        log "Activating the virtual environment at $VENV_DIR..." "INFO"
         source "$VENV_DIR/bin/activate"
     else
-        log "Error: Virtual environment not found at $VENV_DIR. Exiting."
+        log "Error: Virtual environment not found at $VENV_DIR. Exiting." "ERROR"
         exit 1
     fi
 }
@@ -29,7 +38,7 @@ activate_venv() {
 # Function to deactivate the virtual environment
 deactivate_venv() {
     if [[ "$VIRTUAL_ENV" != "" ]]; then
-        log "Deactivating the virtual environment..."
+        log "Deactivating the virtual environment..." "INFO"
         deactivate
     fi
 }
@@ -38,10 +47,10 @@ deactivate_venv() {
 set_permissions() {
     local target_dir=$1
     local user_group=$2
-    log "Setting permissions for $target_dir..."
+    log "Setting permissions for $target_dir..." "INFO"
 
     sudo chown -R "$user_group" "$target_dir" || {
-        log "Failed to change ownership of $target_dir for $user_group."
+        log "Failed to change ownership of $target_dir for $user_group." "ERROR"
         exit 1
     }
     sudo find "$target_dir" -type d -exec chmod 755 {} \;
@@ -52,10 +61,10 @@ set_permissions() {
 restart_or_start_service() {
     local service_name=$1
     if systemctl is-active --quiet "$service_name"; then
-        log "$service_name is running. Restarting $service_name..."
+        log "$service_name is running. Restarting $service_name..." "INFO"
         sudo systemctl restart "$service_name"
     else
-        log "$service_name is not running. Starting $service_name..."
+        log "$service_name is not running. Starting $service_name..." "INFO"
         sudo systemctl start "$service_name"
     fi
 }
@@ -66,7 +75,7 @@ process_project() {
     local nginx_html_dir="$project_dir/nginx"
     local html_dir="$project_dir/app/static"
 
-    log "Processing project: $project_dir"
+    log "Processing project: $project_dir" "INFO"
 
     # Activate the fixed virtual environment
     activate_venv
@@ -78,7 +87,7 @@ process_project() {
 
     # Install necessary Python packages
     pip install -r requirements_temp.txt || {
-        log "Failed to install some packages. Please check the virtual environment setup."
+        log "Failed to install some packages. Please check the virtual environment setup." "ERROR"
         deactivate_venv
         exit 1
     }
@@ -87,7 +96,7 @@ process_project() {
     rm requirements_temp.txt
 
     deactivate_venv
-    log "Python packages installed successfully for project: $project_dir."
+    log "Python packages installed successfully for project: $project_dir." "INFO"
 
     # Copy static files and update Nginx configuration
     update_static_files_and_nginx "$project_dir" "$nginx_html_dir"
@@ -95,7 +104,7 @@ process_project() {
 
     # Process all HTML files in the static directory
     for html_file in "$html_dir/"*.html; do
-        log "Processing HTML file: $html_file"
+        log "Processing HTML file: $html_file" "INFO"
         project_name=$(basename "$html_file" _layout.html)
         colab_link=$(grep -oP 'https://colab\.research\.google\.com/github/[^"]+' "$html_file" || true)
 
@@ -106,7 +115,7 @@ process_project() {
             scripts_dir="$project_path/src/scripts"
             output_dir="$nginx_html_dir/$project_name"
 
-            log "Processing project: $project_name"
+            log "Processing project: $project_name" "INFO"
             if [ -d "$project_path" ]; then
                 sudo mkdir -p "$output_dir"
                 convert_notebooks "$notebook_dir" "$output_dir"
@@ -115,10 +124,10 @@ process_project() {
                 embed_notebook_into_layout "$output_dir" "$html_file"
                 place_files "$output_dir" "/var/www/htmx_website/$project_name"
             else
-                log "Mission directory $project_path does not exist or is not accessible. Skipping..."
+                log "Mission directory $project_path does not exist or is not accessible. Skipping..." "WARNING"
             fi
         else
-            log "No Colab link found in $project_name. Skipping..."
+            log "No Colab link found in $project_name. Skipping..." "INFO"
         fi
     done
 
@@ -127,288 +136,167 @@ process_project() {
     restart_or_start_service "htmx_website.service"
     restart_or_start_service "nginx"
 
-    log "Deployment complete for project: $project_dir!"
+    log "Deployment complete for project: $project_dir!" "INFO"
 }
 
 # Function to update Nginx static files and configuration
 update_static_files_and_nginx() {
     local project_dir="$1"
     local nginx_html_dir="$2"
+    local backup_timestamp=$(date +'%Y%m%d%H%M%S')
 
-    log "Copying updated static files for project: $project_dir"
+    log "Copying updated static files for project: $project_dir" "INFO"
     sudo cp -r "$project_dir/app/static/"* "$nginx_html_dir/"
 
-    log "Validating new Nginx configuration..."
-    if sudo nginx -t -c "$project_dir/nginx/htmx_website"; then
-        log "New Nginx configuration is valid."
+    log "Backing up current Nginx configuration..." "INFO"
+    if [ -f /etc/nginx/sites-available/htmx_website ]; then
+        sudo cp /etc/nginx/sites-available/htmx_website /etc/nginx/sites-available/htmx_website.bak.$backup_timestamp
+    fi
 
-        if [ -f /etc/nginx/sites-available/htmx_website ]; then
-            log "Backing up existing Nginx configuration..."
-            sudo cp /etc/nginx/sites-available/htmx_website /etc/nginx/sites-available/htmx_website.bak
-        fi
+    log "Validating new Nginx configuration..." "INFO"
+    if sudo nginx -t -c "$project_dir/nginx/htmx_website"; then
+        log "New Nginx configuration is valid. Applying it now." "INFO"
 
         sudo cp "$project_dir/nginx/htmx_website" /etc/nginx/sites-available/htmx_website
 
         if [ ! -L /etc/nginx/sites-enabled/htmx_website ]; then
-            log "Creating symbolic link for Nginx configuration..."
+            log "Creating symbolic link for Nginx configuration..." "INFO"
             sudo ln -s /etc/nginx/sites-available/htmx_website /etc/nginx/sites-enabled/
         fi
 
-        log "Testing and reloading Nginx..."
-        if sudo nginx -t; then
-            sudo systemctl reload nginx
-        else
-            log "Error: Nginx configuration test failed after copying the new configuration."
-            log "Restoring the previous configuration..."
-            sudo cp /etc/nginx/sites-available/htmx_website.bak /etc/nginx/sites-available/htmx_website
-            sudo nginx -t && sudo systemctl reload nginx
-        fi
+        log "Reloading Nginx with the new configuration..." "INFO"
+        sudo systemctl reload nginx
     else
-        log "Error: The new Nginx configuration is invalid. Falling back to the old configuration."
-        [ ! -f /etc/nginx/sites-available/htmx_website ] && {
-            sudo cp "$project_dir/nginx/htmx_website" /etc/nginx/sites-available/htmx_website
-            [ ! -L /etc/nginx/sites-enabled/htmx_website ] && sudo ln -s /etc/nginx/sites-available/htmx_website /etc/nginx/sites-enabled/
-        }
+        log "Error: New Nginx configuration is invalid. Reverting to the previous configuration." "ERROR"
+        sudo cp /etc/nginx/sites-available/htmx_website.bak.$backup_timestamp /etc/nginx/sites-available/htmx_website
         sudo nginx -t && sudo systemctl reload nginx
     fi
 }
 
-
 # Function to set up the Flask application
 setup_flask_app() {
-    log "Stopping Flask service (htmx_website.service)..."
+    log "Stopping Flask service (htmx_website.service)..." "INFO"
     
     # Stop the Flask service before making changes
     sudo systemctl stop htmx_website.service || {
-        log "Failed to stop htmx_website.service. Please check the service status."
+        log "Failed to stop htmx_website.service. Please check the service status." "ERROR"
         exit 1
     }
 
     # Backup the existing Flask configuration
     if [ -f /srv/htmx_website/server.py ]; then
-        log "Backing up existing Flask configuration..."
+        log "Backing up existing Flask configuration..." "INFO"
         sudo cp /srv/htmx_website/server.py /srv/htmx_website/server.py.bak || {
-            log "Failed to backup the existing Flask configuration."
+            log "Failed to backup the existing Flask configuration." "ERROR"
             exit 1
         }
     fi
 
     # Copy the new server.py configuration
-    log "Copying new Flask configuration..."
+    log "Copying new Flask configuration..." "INFO"
     sudo cp -r app/server.py /srv/htmx_website/server.py || {
-        log "Failed to copy the new Flask configuration."
+        log "Failed to copy the new Flask configuration." "ERROR"
         restore_flask_config
         exit 1
     }
 
-    
-
     # Validate the new Flask application by starting the service
-    log "Starting Flask service (htmx_website.service) to validate the new configuration..."
+    log "Starting Flask service (htmx_website.service) to validate the new configuration..." "INFO"
     if sudo systemctl start htmx_website.service; then
-        log "Flask service started successfully with the new configuration."
+        log "Flask service started successfully with the new configuration." "INFO"
     else
-        log "Error: Failed to start Flask service with the new configuration. Restoring the previous configuration..."
+        log "Error: Failed to start Flask service with the new configuration. Restoring the previous configuration..." "ERROR"
         restore_flask_config
         sudo systemctl start htmx_website.service || {
-            log "Failed to start Flask service with the restored configuration. Please check the service status."
+            log "Failed to start Flask service with the restored configuration. Please check the service status." "ERROR"
             exit 1
         }
     fi
 
     # Set appropriate permissions for the new configuration
-    # Ssudo chown -R www-data:www-data /srv/htmx_website/server.py
-    # sudo find /srv/htmx_website -type d -exec chmod 755 {} \;
+    set_permissions /srv/htmx_website "www-data:www-data"
 }
 
 # Function to restore the previous Flask configuration
 restore_flask_config() {
-    log "Restoring the previous Flask configuration..."
+    log "Restoring the previous Flask configuration..." "INFO"
     if [ -f /srv/htmx_website/server.py.bak ]; then
         sudo cp /srv/htmx_website/server.py.bak /srv/htmx_website/server.py || {
-            log "Failed to restore the previous Flask configuration."
+            log "Failed to restore the previous Flask configuration." "ERROR"
             exit 1
         }
-        log "Previous Flask configuration restored successfully."
+        log "Previous Flask configuration restored successfully." "INFO"
     else
-        log "No backup configuration found to restore. Aborting."
+        log "No backup found to restore the previous Flask configuration." "ERROR"
         exit 1
     fi
 }
-# Function to convert Jupyter notebooks to HTML with additional debugging
+
+# Function to convert Jupyter notebooks to HTML
 convert_notebooks() {
     local notebook_dir=$1
     local output_dir=$2
+    log "Converting notebooks in $notebook_dir to HTML in $output_dir" "INFO"
 
-    echo "Converting notebooks in $notebook_dir to HTML..."
-    mkdir -p "$output_dir"
-
-    activate_venv
-
-    # List all notebooks in the directory to confirm they exist
-    echo "Listing notebooks in $notebook_dir:"
-    ls -l "$notebook_dir"/*.ipynb
-
-    for notebook in "$notebook_dir"/*.ipynb; do
-        if [ -f "$notebook" ]; then
-            echo "Converting $notebook..."
-
-            # Use the full path to jupyter-nbconvert and sudo with environment preservation
-            sudo -E /srv/htmx_website/venv/bin/jupyter-nbconvert "$notebook" --to html --output-dir "$output_dir" || {
-                echo "Failed to convert $notebook due to permissions or other errors. Skipping..."
-                continue
-            }
-            echo "Converted: $notebook to $output_dir"
-        else
-            echo "No notebooks found in $notebook_dir."
-        fi
-    done
-
-    deactivate_venv
-
-    
+    if [ -d "$notebook_dir" ]; then
+        jupyter nbconvert --to html --output-dir="$output_dir" "$notebook_dir"/*.ipynb
+        log "Notebook conversion completed." "INFO"
+    else
+        log "Notebook directory $notebook_dir does not exist. Skipping conversion." "WARNING"
+    fi
 }
 
 # Function to update Sphinx documentation
 update_sphinx_docs() {
-    local scripts_dir=$1
-    local output_dir="$2/scripts"
+    local sphinx_dir=$1
+    local output_dir=$2
+    log "Updating Sphinx documentation from $sphinx_dir to $output_dir" "INFO"
 
-    echo "Updating Sphinx documentation for scripts in $scripts_dir..."
-
-    if [ ! -d "$scripts_dir" ]; then
-        echo "Scripts directory $scripts_dir does not exist. Skipping documentation generation."
-        return
+    if [ -d "$sphinx_dir" ]; then
+        (cd "$sphinx_dir" && make html)
+        cp -r "$sphinx_dir/_build/html/"* "$output_dir/"
+        log "Sphinx documentation updated successfully." "INFO"
+    else
+        log "Sphinx documentation directory $sphinx_dir does not exist. Skipping documentation update." "WARNING"
     fi
-
-    local docs_dir="$scripts_dir/docs"
-    mkdir -p "$docs_dir/source"
-
-    # Ensure the script has permissions to write to the docs directory
-    sudo chown -R $(whoami):$(whoami) "$docs_dir"
-    sudo chmod -R u+w "$docs_dir"
-
-    activate_venv
-    export PYTHONPATH="$scripts_dir"
-
-    if [ ! -f "$docs_dir/conf.py" ]; then
-        sphinx-quickstart --quiet -p "Script Documentation" -a "Author" -v 1.0 --ext-autodoc --makefile "$docs_dir"
-        sed -i '1i\import sys, os' "$docs_dir/conf.py"
-        sed -i "/sys\.path\.insert/a sys.path.insert(0, os.path.abspath('../'))" "$docs_dir/conf.py"
-    fi
-
-    sphinx-apidoc -o "$docs_dir/source" "$scripts_dir"
-    generate_index_rst "$docs_dir/source"
-
-    make -C "$docs_dir" clean
-    make -C "$docs_dir" html || {
-        echo "Failed to build documentation with Sphinx. Check the configuration."
-        deactivate_venv
-        return
-    }
-    deactivate_venv
-
-    move_generated_docs "$docs_dir/_build/html" "$output_dir"
 }
 
-
-# Function to generate a proper index.rst for Sphinx documentation
-generate_index_rst() {
-    local source_dir=$1
-
-    # Check if the source directory exists
-    if [ ! -d "$source_dir" ]; then
-        echo "Source directory $source_dir does not exist. Skipping index generation."
-        return
-    fi
-
-    echo "Generating index.rst in $source_dir..."
-
-    # Create the index.rst file
-    {
-        echo ".. toctree::"
-        echo "   :maxdepth: 2"
-        echo "   :caption: Contents:"
-        echo ""
-    } > "$source_dir/index.rst"
-
-    # Append .rst files to the index.rst
-    for rst_file in "$source_dir/"*.rst; do
-        rst_filename=$(basename "$rst_file" .rst)
-        echo "   $rst_filename" >> "$source_dir/index.rst"
-    done
-
-    echo "index.rst generated successfully in $source_dir."
-}
-
-# Function to move generated documentation to the correct output directory
-move_generated_docs() {
-    local source_dir="$1"
-    local destination_dir="$2"
-
-    echo "Moving generated documentation from $source_dir to $destination_dir..."
-    if [ -d "$destination_dir" ]; then
-        echo "Cleaning existing directory $destination_dir..."
-        sudo rm -rf "$destination_dir"/*  # Clean up the existing directory
-    fi
-
-    sudo mkdir -p "$destination_dir"
-    sudo mv "$source_dir"/* "$destination_dir/"
-    
-    echo "Moved generated docs to: $destination_dir"
-}
-
-# Function to embed Jupyter notebooks into the layout
+# Function to embed notebook into layout
 embed_notebook_into_layout() {
-    local output_dir="$1"
-    local layout_file="$2"  # Layout file passed as a parameter
+    local output_dir=$1
+    local layout_file=$2
 
-    log "Embedding notebooks into layout using $layout_file"
+    log "Embedding notebook files into layout $layout_file..." "INFO"
 
-    for notebook_html in "$output_dir"/*.html; do
-        if [ -f "$notebook_html" ]; then
-            local output_html="$output_dir/$(basename "$notebook_html"| tr '[:upper:]' '[:lower:]')"
-            log "Embedding $notebook_html into layout..."
+    notebook_files=$(find "$output_dir" -type f -name '*.html')
 
-            if [ -f "$layout_file" ]; then
-                sudo cat "$layout_file" > "$output_html" || {
-                    log "Failed to embed $layout_file into $output_html."
-                    continue
-                }
-                log "Embedded $layout_file into layout successfully."
-            else
-                log "Layout file $layout_file does not exist. Skipping embedding for $output_html."
-            fi
-        else
-            log "No HTML files found in $output_dir."
-        fi
+    for notebook_file in $notebook_files; do
+        notebook_content=$(cat "$notebook_file")
+        sed -i "/<!-- NOTEBOOK_PLACEHOLDER -->/r $notebook_file" "$layout_file"
     done
+
+    log "Notebooks embedded into layout successfully." "INFO"
 }
 
-# Function to place files in the final destination
+# Function to place final files into the destination directory
 place_files() {
-    local source_dir="$1"
-    local destination_dir="$2"
-
-    log "Placing files from $source_dir to $destination_dir..."
-    if [ -d "$destination_dir" ]; then
-        log "Cleaning existing directory $destination_dir..."
-        sudo rm -rf "$destination_dir"/*  # Clean up the existing directory
-    fi
+    local output_dir=$1
+    local destination_dir=$2
+    log "Placing files from $output_dir to $destination_dir" "INFO"
 
     sudo mkdir -p "$destination_dir"
-    sudo cp -r "$source_dir/"* "$destination_dir/"
-    set_permissions "$destination_dir" "www-data:www-data"
-    log "Files placed in: $destination_dir"
+    sudo cp -r "$output_dir"/* "$destination_dir/"
+    log "Files placed into $destination_dir successfully." "INFO"
 }
 
-# Main deployment function
-deploy() {
-    # Loop through all directories matching the pattern (e.g., mission*, toto*, etc.)
-    for project_dir in "$BASE_DIR"/datascience_adventure; do
-        [ -d "$project_dir" ] && process_project "$project_dir" || log "Skipping non-directory: $project_dir"
-    done
-}
+# Main script execution starts here
+log "Starting deployment script..." "INFO"
 
-# Execute the deployment process for all projects
-deploy
+# Process each project in the base directory
+for project in "$BASE_DIR"/*; do
+    if [ -d "$project" ]; then
+        process_project "$project"
+    fi
+done
+
+log "Deployment script completed successfully!" "INFO"
