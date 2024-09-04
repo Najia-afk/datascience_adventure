46e02675e04ac5553a17006e1566df93701d3fac
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2024-09-04 22:15:50 +0200

    test

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # Serve the main index.html file
-    return app.send_static_file('index.html')
+    return app.send_static_file('/var/www/htmx_website/index.html')
 
 @app.route("/load-datascientist-training-project-3")
 def load_project_3():
@@ -13,7 +13,7 @@ def load_project_3():
     <div class="article">
         <h2>Mission 3: Dataset exploration for a public health agency</h2>
         <p>The Public Health Agency wishes to improve the quality of Open Food Facts data, an open source database is made available to individuals and organizations to allow them to know the nutritional quality of products.</p>
-        <a href="/mission3/mission3.html" target="_blank">Go to mission 3</a>
+        <a href="/var/www/htmx_website/mission3/mission3.html" target="_blank">Go to mission 3</a>
     </div>
     """
     return render_template_string(content)
