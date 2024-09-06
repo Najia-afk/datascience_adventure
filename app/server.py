66b26a1a5d723d2b37051f0257b9c845f26d3fae
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2024-09-06 15:50:06 +0200

    test

--- app/server.py
+++ app/server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="/var/www/htmx_website/")
 
-
 @app.route('/header')
 def header():
     return render_template('header.html')
@@ -19,11 +18,9 @@ def summary():
 def load_home():
     return render_template('home.html')
 
-
 @app.route('/mission3')
 def mission3():
-    return render_template('/mission3/mission3.html')
-
+    return render_template('mission3/mission3.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
