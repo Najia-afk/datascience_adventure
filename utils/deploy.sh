76bd0647604dfcad0e4740333ca63a6711ac69ff
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2024-09-06 15:28:03 +0200

    test

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -120,7 +120,7 @@ process_project() {
     sudo mkdir -p "/var/www/htmx_website/templates"
     sudo mkdir -p "/var/www/htmx_website/styles"
     
-    set_permissions "$website_html_dir" "ubuntu:ubuntu"
+    set_permissions "$website_html_dir" "www-data:www-data"
 
     log "Copying static files from $html_dir to $website_html_dir" "INFO"
     
