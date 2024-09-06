f88e60954558f7974d6739604f62a11f7672c476
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2024-09-06 14:44:16 +0200

    test

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, abort
+from flask import Flask, send_from_directory, abort, render_template
 
 app = Flask(__name__, static_folder="/var/www/htmx_website/")
 
@@ -7,23 +7,26 @@ def index():
     # Serve the main index.html file
     return app.send_static_file('index.html')
 
-@app.route("/load-home")
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+@app.route('/footer')
+def footer():
+    return render_template('footer.html')
+
+@app.route('/summary')
+def summary():
+    return render_template('summary.html')
+
+@app.route('/load-home')
 def load_home():
-    # Serve the main index.html file for HTMX requests
-    return app.send_static_file('index.html')
+    return render_template('home.html')
 
-@app.route("/mission3")
-def load_mission3():
-    # Serve the mission3.html file for HTMX requests
-    return app.send_static_file('mission3/mission3.html')
 
-@app.route("/mission3/scripts/<path:filename>")
-def serve_scripts(filename):
-    # Serve HTML files from the 'scripts' directory within 'mission3'
-    try:
-        return send_from_directory('mission3/scripts', filename)
-    except FileNotFoundError:
-        abort(404)
+@app.route('/mission3')
+def mission3():
+    return render_template('/mission3/mission3.html')
 
 @app.route("/load-contact")
 def load_contact():
@@ -35,9 +38,6 @@ def toggle_dark_mode():
     # Dummy example for dark mode toggle, replace with actual logic
     return "<style>body { background-color: #333; color: #fff; }</style>"
 
-@app.route("/test")
-def test():
-    return "<p>This is a test response.</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
