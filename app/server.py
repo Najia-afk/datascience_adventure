92d646823d980d21fb5bfeb1aaeef8b6c2be1b2b
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2024-09-12 14:12:52 +0200

    nginx rework

--- app/server.py
+++ app/server.py
@@ -36,6 +36,10 @@ def load_home():
 def mission3():
     return render_template('mission3/mission3.html')
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
 
